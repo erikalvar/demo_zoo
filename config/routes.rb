3cb5b390f180cb51c1968340a36f2829ed70a378
erikalvar <erikalvarbass@gmail.com>
2020-10-29 18:30:17 -0400

    adds upcase queryy param to sounds

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/animals" => "animals#first_animal"
     get "/all_animals" => "animals#all_animals"
+
+    get "/sound_caps" => "sounds#all_caps"
   end
 end
