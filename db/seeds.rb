3977cb510870f82028d6a30041199bfd5ac23e92
erikalvar <erikalvarbass@gmail.com>
2020-10-29 18:04:53 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Animal.create(species: "cow", age: 4, food: "grass")
+# Animal.create(species: "chick", age: 2, food: "corn")
+# Animal.create(species: "snake", age: 4, food: "mice")
